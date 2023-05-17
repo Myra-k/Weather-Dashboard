cf6a65e8ed35579005c75f47163bb09ade42c2fb
myra-k <myrakhatoon90@gmail.com>
2023-05-17 15:49:17 +0100

    draft

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,6 @@
 var dailyForecastNum = 5;
-var baseURL = "https://api.openweathermap.org";
-var weatherApiKey= "26cbab91dad3fbd1df78b7fdca8d6eae";
+var baseURL = "http://api.openweathermap.org";
+var weatherApiKey = "d88475fbbf0b433a0f9e5f2aa5fbe29a";
 
 
 var forecasts = document.getElementById("forecast-day")
@@ -32,47 +32,68 @@ let locError = (text) => {
 const lookupLocation = (search) => {
 
     var apiUrl = `${baseURL}/geo/1.0/direct?q=${search}&limit=5&appid=${weatherApiKey}`;
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
+    fetch( apiUrl )
+        .then(function (response){
+
+            return response.json()
+        })  
+        .then (function(data){
 
             console.log(data);
+            getweather(data[0])
+        })
 
-            // Pick the First location from the results
-            var lat = data[0].lat;
-            var lon = data[0].lon;
-
-
-            var myData = {
-                name: data[0].name,
-                country: data[0].country,
-                lat: data[0].lat,
-                lon: data[0].lon
-            }
-
-            console.log(myData);
+     
 
 
-            // the Weather for the cached location
-             var apiUrl = `${baseURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
-             console.log(apiUrl);
-             fetch(apiUrl)
-                 .then(response => response.json())
-                 .then(data => {
+        }
+        
+        function getweather(location) {
+                let { lat, lon } = location
+                let city = location.name
 
-                     console.log(data);
 
-                   
 
-                     displayCurrentWeather(data);
 
-                     displayWeatherForecast(data);
-                    
+                // Pick the First location from the results
+                // var lat = data[0].lat;
+                // var lon = data[0].lon;
+
+
+                // var myData = {
+                //     name: data[0].name,
+                //     country: data[0].country,
+                //     lat: data[0].lat,
+                //     lon: data[0].lon
+
+
+                // console.log(myData);
+
+
+                // the Weather for the cached location
+                var apiUrl = `${baseURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
+                console.log(apiUrl);
+                fetch(apiUrl)
+                    .then(function (response){
+                        return response.json()
+
+                    })
+                    .then(function(data) {
+
+                        console.log(data);
+
+
+
+                        displayCurrentWeather(data);
+
+                        displayWeatherForecast(data);
+
+                  
+
+                displayWeather(data);
                  });
-
-                displayWeather(myData);
-        });
-}
+            }
+// )}
 
 var displayCurrentWeather = (weatherData) => {
     let currWeather = weatherData.current;
@@ -80,61 +101,66 @@ var displayCurrentWeather = (weatherData) => {
     document.getElementById('temp_val').textContent = `${currWeather.temp}°`;
     document.getElementById('humid_val').textContent = `${currWeather.wind_speed}MPH`;
     document.getElementById('wind_val').textContent = `${currWeather.humidity}%`;
- }
+}
 
 var displayWeatherForecast = (weatherData) => {
 
     var dailyData = weatherData.daily;
 
-    document.getElementById('forecast').style.display ='block';
+    //     var day1 = new Date(dailyforecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
+    // console.log(day1);
+    //     $('#day1day_value').text(day1);
+    //     document.getElementById('day1temp_vale').textContent=`${dailyForecastNum.temp.day}•`;
 
-    var forecastlist = document.getElementById('forecast-day');
-    forecastlist.innerHTML = '';
+    // document.getElementById('forecast').style.display ='block';
 
-    for (let i = 0; i < dailyForecastNum; i++) {
-        
-        var newForecast=document.getElementById('div');
-        newForecast.classList.add('forecast-day');
-        newForecast.innerHTML = `<div class="weather-info">
-        <div class="date">
-            <span>${day}</span>
-        </div>
-        <div class="temperature">
-           <span>${temp}</span>
-       </div>
-       <div class="wind">
-           <span>${wind}</span>
-       </div>
-      <div class="humidity">
-          <span>${humidity}</span>
-     </div> 
-     </div>`;
+    // var forecastlist = document.getElementById('forecast-day');
+    // forecastlist.innerHTML = '';
 
-     forecastlist.appendChild(newForecast);
+    //     for (let i = 0; i < dailyForecastNum; i++) {
+
+    //         var newForecast=document.getElementById('div');
+    //         newForecast.classList.add('forecast-day');
+    //         newForecast.innerHTML = `<div class="weather-info">
+    //         <div class="date">
+    //             <span>${day}</span>
+    //         </div>
+    //         <div class="temperature">
+    //            <span>${temp}</span>
+    //        </div>
+    //        <div class="wind">
+    //            <span>${wind}</span>
+    //        </div>
+    //       <div class="humidity">
+    //           <span>${humidity}</span>
+    //      </div> 
+    //      </div>`;
+
+    //      forecastlist.appendChild(newForecast);
 
 
-        var dailyforecast = dailyData[i];
-        var day = new Date(dailyforecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
-        var temp = `${dailyforecast.temp.day}°`;
-        var humidity = `${dailyforecast.humidity}%`;
-        var wind= `${dailyforecast.wind_speed}MPH`;
+    //         var dailyforecast = dailyData[i];
+    //         var day = new Date(dailyforecast.dt * 1000).toLocaleDateString('en-GB', { weekday: 'long'});
+    //         var temp = `${dailyforecast.temp.day}°`;
+    //         var humidity = `${dailyforecast.humidity}%`;
+    //         var wind= `${dailyforecast.wind_speed}MPH`;
 
-       
-    }
 
- }
+    //     }
 
- const getTheWeather = (lat, lon) => {
+}
 
-     var apiUrl = `${baseURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
+const getTheWeather = (lat, lon) => {
 
-     fetch(apiUrl)
-     .then(response => response.json())
-     .then(data => {
-         displayCurrentWeather(data);
+    var apiUrl = `${baseURL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${weatherApiKey}`;
 
-         displayWeatherForecast(data);
-     })
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayCurrentWeather(data);
+
+            displayWeatherForecast(data);
+        })
 }
 
 var displayWeather = (weatherData) => {
